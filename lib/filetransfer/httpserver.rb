0346660e1a704c69e93b71da2cacf36aec4a31de
Matthias Lee <matthias@matthiasclee.com>
2023-03-24 16:54:18 -0700

    Removed extra puts

--- lib/filetransfer/httpserver.rb
+++ lib/filetransfer/httpserver.rb
@@ -4,7 +4,6 @@ module FileTransfer
       read = true
       while read
         x= client.gets
-        puts x.dump
         read = false if x == "\r\n" || !x
       end
       client.puts "HTTP/1.0 404\r\nContent-Type: text/plain\r\n\r\nasd"
