8c6cafce3e9126eb6f7f9aec1085ff5a3539ec3c
Matthias Lee <matthias@matthiasclee.com>
2023-03-24 16:54:04 -0700

    HTTP server works

--- lib/filetransfer/httpserver.rb
+++ lib/filetransfer/httpserver.rb
@@ -1,7 +1,14 @@
 module FileTransfer
   module HTTPserver
     def self.handle_client(initdata, client)
-      puts "asdasd" 
+      read = true
+      while read
+        x= client.gets
+        puts x.dump
+        read = false if x == "\r\n" || !x
+      end
+      client.puts "HTTP/1.0 404\r\nContent-Type: text/plain\r\n\r\nasd"
+      client.close
     end
   end
 end
