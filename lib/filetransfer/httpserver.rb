10f255bf2804b97d43e5e544b04a0607f39e622c
Matthias Lee <matthias@matthiasclee.com>
2023-03-24 17:45:34 -0700

    Misc

--- lib/filetransfer/httpserver.rb
+++ lib/filetransfer/httpserver.rb
@@ -2,13 +2,15 @@ module FileTransfer
   module HTTPserver
     def self.handle_client(initdata, client)
       path = initdata[1]
+      file = $active_transfers[path]
+      client.close if !file
 
       read = true
       while read
         x= client.gets
         read = false if x == "\r\n" || !x
       end
-      client.puts "HTTP/1.0 404\r\nContent-Type: text/plain\r\n\r\n#{File.read($active_transfers[path][:path])}"
+      client.puts "HTTP/1.0 200\r\nContent-Type: application/octet-stream\r\nContent-Disposition: attachment; filename=\"#{file[:name]}\"\r\n\r\n#{File.read(file[:path])}"
       client.close
     end
   end
