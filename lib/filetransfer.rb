6da87f331e8262747f77326ff6ec8ca0a5845b8a
Matthias Lee <matthias@matthiasclee.com>
2023-03-25 07:00:38 -0700

    Bump

--- lib/filetransfer.rb
+++ lib/filetransfer.rb
@@ -5,7 +5,7 @@ module FileTransfer
   @@files = ['server.rb', 'notifier.rb', 'httpserver.rb', 'ftserver.rb']
 
   def self.version
-    "0.0.1"
+    "0.0.2"
   end
 
   def self.file_paths(relative:false)
