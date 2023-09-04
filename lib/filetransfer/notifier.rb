b5727f33f2f2d0791114563f6bc6843f1fb3b6ba
Matthias Lee <matthias@matthiasclee.com>
2023-09-03 17:19:25 -0700

    Fix RCE

--- lib/filetransfer/notifier.rb
+++ lib/filetransfer/notifier.rb
@@ -1,15 +1,15 @@
 module FileTransfer
   module Notifier
     def self.file_transfer_notification(filename:, source:, filesize:, accept_link:)
-      notification_title = "Incoming file transfer from #{source}"
-      fileinfo = "Name: #{filename}\nSize: #{filesize}"
-      notification_body = "#{fileinfo}\nWould you like to accept?"
-      notification_command = "notify-send '#{notification_title}' \"#{notification_body}\""
+      notification_title = "Incoming file transfer from #{source}".gsub("\"", "")
+      fileinfo = "Name: #{filename}\nSize: #{filesize}".gsub("\"", "")
+      notification_body = "#{fileinfo}\nWould you like to accept?".gsub("\"", "")
+      notification_command = "notify-send '#{notification_title}' \"#{notification_body}\"".gsub("\"", "")
       #system(notification_command)
 
       x=system("zenity --question --icon-name=document-save-symbolic.symbolic --no-wrap --text=\"#{notification_body}\" --title='#{notification_title}'")
       if x
-        file_location = `zenity --title="Save file" --file-selection --save --filename=#{filename.chomp}`.chomp
+        file_location = `zenity --title="Save file" --file-selection --save --filename=#{filename.chomp.gsub("\"", "")}`.chomp
         if file_location == ""
           f=open(accept_link + "/decline")
           f.read
